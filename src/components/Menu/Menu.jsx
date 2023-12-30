3eb24f405829a2cd0ab6a0c39f597b7c94cd8c61
robin-dc <robindelacruz725@gmail.com>
2023-12-30 16:39:23 +0800

    feat: Added loading on signout

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -12,6 +12,7 @@ import { removeList } from '../../features/FireStoreSlice'
 const Menu = () => {
     const navigate = useNavigate()
     const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    const [isLogout, setIsLogout] = useState(false)
     const {isScrolled} = useScroll()
     const [photoURL, setPhotoURL] = useState(null)
     const [name, setName] = useState(null)
@@ -44,17 +45,28 @@ const Menu = () => {
     }
 
     const handleSignout = () => {
-        localStorage.removeItem("userData")
-        localStorage.removeItem("token")
-        logOut()
-        dispatch(removeList())
-        navigate("/signin")
+        setIsLogout(true)
+        setTimeout(() => {
+            localStorage.removeItem("userData")
+            localStorage.removeItem("token")
+            logOut()
+            dispatch(removeList())
+            setIsLogout(false)
+            navigate("/signin")
+        }, 2000)
     }
   return (
     <div className='relative'>
-        <div className="flex gap-[0.3rem] md:gap-[0.5rem] items-center cursor-pointer" onClick={toggleDropdown}>
-            <img src="/images/default-red.png" alt="users" className='rounded-md w-[2.1rem] md:w-[2.5rem]'/>
-            <IoIosArrowDown className={`${isDropdownOpen && "rotate-[180deg]"} transition-all duration-200 text-[1rem] md:text-[1.2rem]`}/>
+        {isLogout && <div className='min-h-screen bg-[#0000005b] flex justify-center items-center fixed top-0 bottom-0 right-0 left-0'>
+            <svg  width="80px" height="80px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+                <circle cx="50" cy="50" fill="none" stroke="#E9292C" stroke-width="6" r="35" stroke-dasharray="164.93361431346415 56.97787143782138">
+                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+                </circle>
+            </svg>
+        </div>}
+        <div className="flex gap-[0.3rem] lg:gap-[0.5rem] items-center cursor-pointer" onClick={toggleDropdown}>
+            <img src="/images/default-red.png" alt="users" className='rounded-md w-[2.1rem] lg:w-[2.5rem]'/>
+            <IoIosArrowDown className={`${isDropdownOpen && "rotate-[180deg]"} transition-all duration-200 text-[1rem] lg:text-[1.2rem]`}/>
         </div>
 
         {isDropdownOpen && <div className='border-2 border-[#303030af] bg-black rounded-lg absolute mt-1 right-0 min-w-[230px] text-[0.9rem]'>
